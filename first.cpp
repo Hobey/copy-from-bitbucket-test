8808008217de3daa1606baece72a3b4b102c7aa5
Hobey <rubengpeter@gmail.com>
2017-04-07 23:18:27 +0200

    vector stuff

--- first.cpp
+++ first.cpp
@@ -2,14 +2,29 @@
 #include "windows.h"
 #include "stdio.h"
 
-int main (int argc, char** argv) {
+struct vector2 {
+    float x;
+    float y;        
+};
 
+static void
+print(vector2 v) {
+    printf("x %f, y %f\n", v.x, v.y);
+}
+
+int main (int argc, char** argv) {
+    
     printf("Hello, world!\n");
 
     for (int foo = 5; foo < 11; foo++) {
         printf("foo = %d\n", foo);
     }
 
+    vector2 v;
+    v.x = 23;
+    v.y = v.x * 1.2;
+    print(v);
+    
     printf("Done\n");
     
     return 0;
