e5facb99e941dc2e0856e9ba9e2e311a186ee23b
Hobey <rubengpeter@gmail.com>
2017-04-07 23:27:04 +0200

    renamed previous foo to bar

--- first.cpp
+++ first.cpp
@@ -18,8 +18,8 @@ int main (int argc, char** argv) {
     
     printf("Hello, world!\n");
 
-    for (int foo = 5; foo < 11; foo++) {
-        printf("foo = %d\n", foo);
+    for (int bar = 5; bar < 11; bar++) {
+        printf("bar = %d\n", bar);
     }
 
     vector2 v;
