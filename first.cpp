1fa2bc2de94b73a9a7db5565affbdf44904dee80
Hobey <rubengpeter@gmail.com>
2017-04-07 02:12:44 +0200

    printing loop from 5 to 11

--- first.cpp
+++ first.cpp
@@ -6,7 +6,7 @@ int main (int argc, char** argv) {
 
     printf("Hello, world!\n");
 
-    for (int foo = 3; foo < 8; foo++) {
+    for (int foo = 5; foo < 11; foo++) {
         printf("foo = %d\n", foo);
     }
 
